dfb6f672d4070fefb011aaa606a3acc0aea8b9b5
Tanya Reilly <tanya.reilly@gmail.com>
2016-09-22 12:05:02 -0400

    Better regexp.

--- content.js
+++ content.js
@@ -8,7 +8,13 @@ for (var i = 0; i < elements.length; i++) {
 
     if (node.nodeType === 3) {
       var text = node.nodeValue;
-      var replacedText = text.replace(/Trump/gi, "Ivanka's Dad");
+      var replacedText = text.replace(/\b(\w+( J)? )?Trump\b/gi, function repl(match) {
+        var names = ["donald trump", "donald j trump", "trump"]
+        if (names.indexOf(match.toLowerCase()) == -1) {
+          return match
+        }
+        return "Ivanka's Dad"
+      })
 
       if (replacedText !== text) {
         element.replaceChild(document.createTextNode(replacedText), node);
